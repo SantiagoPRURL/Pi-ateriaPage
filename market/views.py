66677e580753bbf97b077e1363cadf58c9be1d70
SantiagoPRURL <santiagopr123@gmail.com>
2025-02-08 11:51:11 -0500

    Comienzo de Filtros

--- market/views.py
+++ market/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from APIMain.models import Product
+
+
 
 # Create your views here.
 
 def MarketView(request):
     return render(request, "marketPage.html")
+
+
+class ProductFilterView(APIView):
+    def get(self, request):
+        return Response({"message":"Hi"})
